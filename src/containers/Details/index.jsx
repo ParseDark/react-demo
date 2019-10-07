52194064347b29b00932f9a6eccf573e69ac5868
韦海东 <13597213421@163.com>
2019-10-07 17:20:18 +0800

    fix: hook化Details组件.

--- src/containers/Details/index.jsx
+++ src/containers/Details/index.jsx
@@ -1,30 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import './index.scss'
 import Axios from 'axios'
 
-class Details extends React.Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            author: 'Mr.Wei',
-            img: 'img',
-            publishDate: 'Hello Project.',
-            title: '你好啊， 世界.',
-            content: 'bilibili.',
-            readCount: 0,
-            favoriteCount: 0,
-            hasFavorite: 0
-        }
-    }
+const Details = props => {
+    const [authorInfo, setAuthorInfo] = useState({
+        author: 'Mr.Wei',
+        img: 'img',
+        publishDate: 'Hello Project.',
+        title: '你好啊， 世界.',
+        content: 'bilibili.',
+        readCount: 0,
+        favoriteCount: 0,
+        hasFavorite: 0
+    })
 
-    fetchDetails = () => {
-        const {id} = this.props.match.params
+    const fetchDetails = () => {
+        const { id } = props.match.params
         Axios({
             url: `/mock/details/${id}`
         }).then(res => {
             const { data, success } = res.data
-            if(success) {
-                this.setState({
+            if (success) {
+                setAuthorInfo({
                     author: data.author,
                     img: data.img,
                     publishDate: data.publishDate,
@@ -38,39 +35,107 @@ class Details extends React.Component {
         })
     }
 
-    componentDidMount() {
-        this.fetchDetails()
-    }
+    useEffect(() => {
+        fetchDetails()
+    }, [props])
 
-    render() {
-        return (
-            <div className="details">
-                <div className="container">
-                    <div className="header">
-                        <a href="" className="imgBox">
-                            <img src={this.state.img} />
-                        </a>
-                        <div className="article-info">
-                            <span className="user-name">{this.state.author}</span>
-                            <span className="publish-date">{this.state.publishDate}</span>
-                        </div>
-                        <h4>{this.state.title}</h4>
+    return (
+        <div className="details">
+            <div className="container">
+                <div className="header">
+                    <a href="" className="imgBox">
+                        <img src={authorInfo.img} />
+                    </a>
+                    <div className="article-info">
+                        <span className="user-name">{authorInfo.author}</span>
+                        <span className="publish-date">{authorInfo.publishDate}</span>
                     </div>
-                    <div className="detail-body">
-                        <div>
-                            {this.state.content}
-                        </div>
-                        <span className="read-counts">{this.state.readCount} 次阅读</span>
-                        <span className="read-counts">{this.state.favoriteCount} 次点赞</span>
+                    <h4>{authorInfo.title}</h4>
+                </div>
+                <div className="detail-body">
+                    <div>
+                        {authorInfo.content}
                     </div>
-                    <div className="detail-footer">
-                        {/* <i>x</i> */}
-                    </div>
+                    <span className="read-counts">{authorInfo.readCount} 次阅读</span>
+                    <span className="read-counts">{authorInfo.favoriteCount} 次点赞</span>
+                </div>
+                <div className="detail-footer">
+                    {/* <i>x</i> */}
                 </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+
+// class Details extends React.Component {
+//     constructor(props) {
+//         super(props)
+//         this.state = {
+//             author: 'Mr.Wei',
+//             img: 'img',
+//             publishDate: 'Hello Project.',
+//             title: '你好啊， 世界.',
+//             content: 'bilibili.',
+//             readCount: 0,
+//             favoriteCount: 0,
+//             hasFavorite: 0
+//         }
+//     }
+
+//     fetchDetails = () => {
+//         const { id } = this.props.match.params
+//         Axios({
+//             url: `/mock/details/${id}`
+//         }).then(res => {
+//             const { data, success } = res.data
+//             if (success) {
+//                 this.setState({
+//                     author: data.author,
+//                     img: data.img,
+//                     publishDate: data.publishDate,
+//                     title: data.title,
+//                     content: data.content,
+//                     readCount: data.readCount,
+//                     favoriteCount: data.favoriteCount,
+//                     hasFavorite: data.hasFavorite
+//                 })
+//             }
+//         })
+//     }
+
+//     componentDidMount() {
+//         this.fetchDetails()
+//     }
+
+//     render() {
+//         return (
+//             <div className="details">
+//                 <div className="container">
+//                     <div className="header">
+//                         <a href="" className="imgBox">
+//                             <img src={this.state.img} />
+//                         </a>
+//                         <div className="article-info">
+//                             <span className="user-name">{this.state.author}</span>
+//                             <span className="publish-date">{this.state.publishDate}</span>
+//                         </div>
+//                         <h4>{this.state.title}</h4>
+//                     </div>
+//                     <div className="detail-body">
+//                         <div>
+//                             {this.state.content}
+//                         </div>
+//                         <span className="read-counts">{this.state.readCount} 次阅读</span>
+//                         <span className="read-counts">{this.state.favoriteCount} 次点赞</span>
+//                     </div>
+//                     <div className="detail-footer">
+//                         {/* <i>x</i> */}
+//                     </div>
+//                 </div>
+//             </div>
+//         )
+//     }
+// }
 
 export default Details
 
