4e85b258740d03aab26629287dfd65e203c7d135
韦海东 <13597213421@163.com>
2019-10-07 16:46:16 +0800

    fix: 完成home组件的hook化.

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 import { Tag, Typography } from 'antd';
 import './index.scss'
 import Axios from 'axios';
@@ -13,6 +13,90 @@ import {
 
 const { Title, Paragraph } = Typography;
 
+const Home = props => {
+    const [tagText, setTagText] = useState(['node.js', 'react', 'vue', 'go'])
+    const [articleArr, setArticleArr] = useState([
+        {
+            title: '之美满思战运次低器天矿第写之有。料收用色转持史府特象观改。么火济它构儿学回量劳会立细任打出。',
+            content: '之美满思战运次低器天矿第写之有。料收用色转持史府特象观改。么火济它构儿学回量劳会立细任打出。',
+            sid: ''
+        }
+    ])
+
+    const toDetails = id => {
+        const { auth, loginModalShow } = props
+        debugger
+        if (auth) {
+            const { history } = props
+            history.push({
+                pathname: `/details/${id}`
+            })
+        } else {
+            loginModalShow()
+        }
+    }
+
+
+    useEffect(() => {
+        const fetchList = (params, isRefresh) => {
+            addLoading()
+            const removeLoadingHook = removeLoading
+            Axios({
+                url: "/mock/list"
+            }).then(res => {
+                const { success } = res.data
+                if (success) {
+                    removeLoadingHook()
+                    let data
+                    if (isRefresh) {
+                        data = res.data.list
+                    } else {
+                        data = articleArr.concat(res.data.list)
+                    }
+                    setArticleArr(data)
+                }
+            })
+        }
+        fetchList({
+            page: 1,
+            type: 0
+        })
+    }, [props])
+
+
+
+    return (
+        <div className="home">
+            <div className="fix-header-nav">
+                {
+                    tagText.map((item, index) => {
+                        return (
+                            <Tag color="magenta" key={index}>{item}</Tag>
+                        )
+                    })
+                }
+            </div>
+
+            <div className="list-warp">
+                {
+                    articleArr.map((item, index) => (
+                        <a className="article-item" key={item.title} onClick={() => toDetails(index)}>
+                            <Title level={4} className="title-box">{item.title}</Title>
+                            <Paragraph className="content-box">
+                                {item.content}
+                            </Paragraph>
+                        </a>
+                    ))
+                }
+                <div className='loadMore'>
+                    <span className='loadMoreText'>
+                        加载更多
+                        </span>
+                </div>
+            </div>
+        </div >
+    )
+}
 
 const mapStateToProps = state => {
     return ({
@@ -29,91 +113,6 @@ const mapDispatchToProps = dispatch => {
         dispatch)
 }
 
-
-
-class Home extends React.Component {
-    constructor() {
-        super()
-        this.state = {
-            articleArr: [{
-                title: '之美满思战运次低器天矿第写之有。料收用色转持史府特象观改。么火济它构儿学回量劳会立细任打出。',
-                content: '之美满思战运次低器天矿第写之有。料收用色转持史府特象观改。么火济它构儿学回量劳会立细任打出。',
-                sid: ''
-            }]
-        }
-
-        this.fetchList = (params, isRefresh) => {
-            addLoading()
-            Axios({
-                url: "/mock/list"
-            }).then(res => {
-                const { success } = res.data
-                if (success) {
-                    removeLoading()
-                    let data
-                    if (isRefresh) {
-                        data = res.data.list
-                    } else {
-                        data = this.state.articleArr.concat(res.data.list)
-                    }
-                    this.setState({
-                        articleArr: data
-                    })
-                }
-            })
-        }
-
-        this.toDetails = id => {
-            const { auth, loginModalShow } = this.props
-            if (auth) {
-                const { history } = this.props
-                history.push({
-                    pathname: `/details/${id}`
-                })
-            } else {
-                loginModalShow()
-            }
-        }
-
-    }
-
-    componentDidMount() {
-        this.fetchList({
-            page: 1,
-            type: 0
-        })
-    }
-    render() {
-        return (
-            <div className="home">
-                <div className="fix-header-nav">
-                    <Tag color="magenta" >推荐</Tag>
-                    <Tag color="geekblue">生活</Tag>
-                    <Tag color="purple">科技</Tag>
-                </div>
-
-                <div className="list-warp">
-                    {
-                        this.state.articleArr.map((item, index) => (
-                            <a className="article-item" key={index} onClick={() => this.toDetails(index)}>
-                                {/* <Title level={2}>设计资源</Title> */}
-                                <Title level={4} className="title-box">{item.title}</Title>
-                                <Paragraph className="content-box">
-                                    {item.content}
-                                </Paragraph>
-                            </a>
-                        ))
-                    }
-                    <div className='loadMore'>
-                        <span className='loadMoreText'>
-                            加载更多
-                        </span>
-                    </div>
-                </div>
-            </div >
-        )
-    }
-}
 export default connect(
     mapStateToProps,
     mapDispatchToProps
